2294ec4a0f74362ad3eb926ba3fd1dcb3769000a
Sixuan <wusixuan2@gmail.com>
2018-11-04 22:01:10 +0000

    finish instance method add_rover

--- mars_rovers/lib/plateau.rb
+++ mars_rovers/lib/plateau.rb
@@ -6,5 +6,14 @@ module MARS_ROVERS
       @occupied = [] # @occupied is an array of coordinates that are occupied by rovers
     end
 
+    def add_rover(coordinate) # place existing rover on to the plateau
+      if coordinate[:x] <= @plateau_dimention[:x] &&
+        coordinate[:y] <= @plateau_dimention[:y] &&
+        coordinate[:x] >= 0 &&
+        coordinate[:y] >= 0
+        @occupied.push(coordinate)
+      end
+    end
+
   end
 end
--- mars_rovers/spec/plateau_spec.rb
+++ mars_rovers/spec/plateau_spec.rb
@@ -5,14 +5,26 @@ describe "Plateau Class" do
   before do
     @plateau = MARS_ROVERS::Plateau.new({x: 5, y: 5})
   end
-  it "should initialize/set attr_reader for instance variable plateau_dimention with argument " do
-    expect(@plateau.plateau_dimention).to eq({x: 5, y: 5})
-    expect(@plateau.plateau_dimention[:x]).to eq(5)
-    expect(@plateau.plateau_dimention[:y]).to eq(5)
+  context 'initializing' do
+    it "should initialize/set attr_reader for instance variable plateau_dimention with argument" do
+      expect(@plateau.plateau_dimention).to eq({x: 5, y: 5})
+    end
+
+    it "should initialize/set attr_reader for instance variable occupied, default value is an empty array" do
+      expect(@plateau.occupied).to eq([])
+    end
   end
 
-  it "should initialize/set attr_reader for instance variable occupied, default value is an empty array" do
-    expect(@plateau.occupied).to eq([])
-  end
+  context 'instance method add_rover' do
+    it "should add coordinate to occupied if coordinate is inside the plateau" do
+      @plateau.add_rover({x: 2, y: 2})
+      expect(@plateau.occupied).to eq([{x: 2, y: 2}])
+    end
 
+    it "should add coordinate to occupied if coordinate is on the edge of plateau" do
+      @plateau.add_rover({x: 0, y: 0})
+      @plateau.add_rover({x: 5, y: 5})
+      expect(@plateau.occupied).to eq([{x: 0, y: 0}, {x: 5, y: 5}])
+    end
+  end
 end
