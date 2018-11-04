d4aa076129a39ce5a9fb07ba61dddfd9289d0ab4
Sixuan <wusixuan2@gmail.com>
2018-11-04 22:33:16 +0000

    testing and finish implementing rotate

--- mars_rovers/lib/rover.rb
+++ mars_rovers/lib/rover.rb
@@ -7,5 +7,31 @@ module MARS_ROVERS
       @y = y
       @orientation = orientation
     end
+
+    def rotate(direction)
+      if direction === 'L'
+        case @orientation
+        when 'N'
+          @orientation = 'W'
+        when 'W'
+          @orientation = 'S'
+        when 'S'
+          @orientation = 'E'
+        when 'E'
+          @orientation = 'N'
+        end
+      else
+        case @orientation
+        when 'N'
+          @orientation = 'E'
+        when 'W'
+          @orientation = 'N'
+        when 'S'
+          @orientation = 'W'
+        when 'E'
+          @orientation = 'S'
+        end
+      end
+    end
   end
 end
--- mars_rovers/spec/rover_spec.rb
+++ mars_rovers/spec/rover_spec.rb
@@ -5,23 +5,70 @@ require "plateau"
 describe "Rover Class" do
   before do
     @plateau = MARS_ROVERS::Plateau.new({x: 5, y: 5})
-    @Rover = MARS_ROVERS::Rover.new(@plateau, 1, 2, 'N')
+    @rover = MARS_ROVERS::Rover.new(@plateau, 1, 2, 'N')
   end
   context 'initializing' do
-    it "should initialize/set attr_reader for plateau with first argument" do
-      expect(@Rover.plateau).to eq(@plateau)
+    it "should initialize/set attr_reader for plateau" do
+      expect(@rover.plateau).to eq(@plateau)
     end
 
-    it "should initialize/set attr_reader for x with the 2nd argument" do
-      expect(@Rover.x).to eq(1)
+    it "should initialize/set attr_reader for x" do
+      expect(@rover.x).to eq(1)
     end
 
-    it "should initialize/set attr_reader for y with the 3rd argument" do
-      expect(@Rover.y).to eq(2)
+    it "should initialize/set attr_reader for y" do
+      expect(@rover.y).to eq(2)
     end
 
-    it "should initialize/set attr_reader for orientation with the 4th argument" do
-      expect(@Rover.orientation).to eq('N')
+    it "should initialize/set attr_reader for orientation" do
+      expect(@rover.orientation).to eq('N')
+    end
+  end
+
+  context "instance method rotate" do
+    before do
+      @rover_west = MARS_ROVERS::Rover.new(@plateau, 1, 2, 'W')
+      @rover_south = MARS_ROVERS::Rover.new(@plateau, 1, 2, 'S')
+      @rover_east = MARS_ROVERS::Rover.new(@plateau, 1, 2, 'E')
+    end
+    it "should rotate to W when instructed with L and current orientation is N" do
+      @rover.rotate('L')
+      expect(@rover.orientation).to eq('W')
+    end
+
+    it "should rotate to S when instructed with L and current orientation is W" do
+      @rover_west.rotate('L')
+      expect(@rover_west.orientation).to eq('S')
+    end
+
+    it "should rotate to E when instructed with L and current orientation is S" do
+      @rover_south.rotate('L')
+      expect(@rover_south.orientation).to eq('E')
+    end
+
+    it "should rotate to N when instructed with L and current orientation is E" do
+      @rover_east.rotate('L')
+      expect(@rover_east.orientation).to eq('N')
+    end
+
+    it "should rotate to E when instructed with R and current orientation is N" do
+      @rover.rotate('R')
+      expect(@rover.orientation).to eq('E')
+    end
+
+    it "should rotate to N when instructed with R and current orientation is W" do
+      @rover_west.rotate('R')
+      expect(@rover_west.orientation).to eq('N')
+    end
+
+    it "should rotate to S when instructed with R and current orientation is E" do
+      @rover_east.rotate('R')
+      expect(@rover_east.orientation).to eq('S')
+    end
+
+    it "should rotate to W when instructed with R and current orientation is S" do
+      @rover_south.rotate('R')
+      expect(@rover_south.orientation).to eq('W')
     end
   end
 end
