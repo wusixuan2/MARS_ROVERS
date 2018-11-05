3c3ba582684185fa767e35eb9e696697ecfe0fad
Sixuan <wusixuan2@gmail.com>
2018-11-05 01:12:38 +0000

    finished testing and implementing create_plateau

--- mars_rovers/lib/instruction.rb
+++ mars_rovers/lib/instruction.rb
@@ -1,9 +1,20 @@
 module MARS_ROVERS
   class Instruction
-    attr_reader :instructions, :final_list_rover
+    attr_reader :instructions, :final_list_rover, :plateau
     def initialize(input)
       @instructions = input.split(/\n/) # convert the instructions from string to arrays
       @final_list_rover = [];
+    end
+
+    def create_plateau
+      x = @instructions.first.split(' ')[0].to_i
+      y = @instructions.first.split(' ')[1].to_i
+      plateau_dimention = {x: x, y: y}
+      @plateau = Plateau.new(plateau_dimention)
+      if !@plateau.plateau_valid?
+        puts 'Invalid plateau coordinate'
+        exit
+      end
     end
 
   end
--- mars_rovers/spec/instruction_spec.rb
+++ mars_rovers/spec/instruction_spec.rb
@@ -1,25 +1,40 @@
 require 'spec_helper'
-# require "rover"
-# require "plateau"
+require "rover"
+require "plateau"
 require "instruction"
 
 describe "Instruction Class" do
   before do
-    # @plateau = MARS_ROVERS::Plateau.new({x: 5, y: 5})
-    # @rover = MARS_ROVERS::Rover.new(@plateau, 1, 2, 'N')
-    # @rover2 = MARS_ROVERS::Rover.new(@plateau, 3, 3, 'E')
     sample_input = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM"
     @instruction = MARS_ROVERS::Instruction.new(sample_input)
   end
 
   context 'initializing' do
     it "should initialize instructions" do
-      puts @instruction.instructions
       expect(@instruction.instructions).to eq(["5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"])
     end
 
     it "should initialize final_list_rover" do
       expect(@instruction.final_list_rover).to eq([])
+    end
+  end
+
+  describe "#create_plateau" do
+    it "create new plateau instance" do
+      @instruction.create_plateau
+      expect(@instruction.plateau).to be_an_instance_of(MARS_ROVERS::Plateau)
+    end
+
+    it "exit if invalid plateau" do
+      invalid_input = "-5 5\n1 2 N\nLMLMLMLMM"
+      @invalid_instruction = MARS_ROVERS::Instruction.new(invalid_input)
+      expect { @invalid_instruction.create_plateau }.to raise_error(SystemExit)
+    end
+
+    it "plateau's dimention is based on instruction" do
+      @instruction.create_plateau
+      expect(@instruction.plateau.plateau_dimention[:x]).to eq(5)
+      expect(@instruction.plateau.plateau_dimention[:y]).to eq(5)
     end
   end
 
