ca68c36764fa25ed20c958acf3092621f484fad0
Sixuan <wusixuan2@gmail.com>
2018-11-05 14:40:49 +0000

    refractor

--- mars_rovers/start.rb
+++ mars_rovers/start.rb
@@ -2,16 +2,23 @@ require './lib/instruction.rb'
 require './lib/rover.rb'
 require './lib/plateau.rb'
 
-begin
-  File.open("./test_input/#{ARGV[0]}")
-rescue
-  puts "failed to open #{ARGV[0]}\n"
-  puts "check the file name and the file has to be inside test_input folder"
-  exit
+if ARGV === []
+  puts "Please specify a file name"
 else
-  sample_input = File.read("./test_input/#{ARGV[0]}")
-  MARS_ROVERS::Instruction.new(sample_input).generate_output
+  begin
+    File.open("./test_input/#{ARGV[0]}")
+  rescue
+    puts "failed to open #{ARGV[0]}\n"
+    puts "check the file name and the file has to be inside test_input folder"
+    exit
+  else
+    sample_input = File.read("./test_input/#{ARGV[0]}")
+    MARS_ROVERS::Instruction.new(sample_input).generate_output
+  end
+
 end
+
+
 
 
 
